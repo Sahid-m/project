daee070f96f320c1b88e4199727a647f6e3d2bbd
Sahid Munjavar <sahidmunjavar.s@gmail.com>
2024-01-15 14:23:25 +0000

    Made Some Changes to CurrenPages

--- src/Components/CurrentProjectsLayout.js
+++ src/Components/CurrentProjectsLayout.js
@@ -16,11 +16,15 @@ export default function CurrentProjectsLayout(props) {
           <h5 className="card-title">{title}</h5>
           <p className="card-text my-2">{description}</p>
           <div className=" my-2 card-text">
+            <span className="fw-bold">Tech Stack:</span>
             {techs.map((techItem) => (
               <button className="btn btn-dark mx-1">{techItem}</button>
             ))}
           </div>
-          <p className="card-text">{TimeLeft}</p>
+          <p className="card-text">
+            <span className="fw-bold">Time to make Project : </span>
+            {TimeLeft}
+          </p>
           <a href={github} className="btn btn-success">
             Github Link
           </a>
